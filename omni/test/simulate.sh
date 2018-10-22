569c4b9e44650537a1ff5ed53fa97c91e39317ab
Kirill Gerasimov <kiriger@gmail.com>
2018-10-22 08:37:25 +0000

    [AWS] Created a branch from a stable checkpoint that integrated wrench viscous at least up to t=18 on OpenModelica 1.12.0~7-g6d56dda

--- omni/test/simulate.sh
+++ omni/test/simulate.sh
@@ -8,7 +8,22 @@ else
   exit -1;
 fi
 
-tmp_file_name="out/simulate_$1.mos";
+if [[ $2 ]]
+then
+  if [[ -d $2 ]] 
+  then
+    echo "dir $2 already exists, please either remove it or specify another one";
+    exit -1;
+  else
+    echo "Going to put stuff to $2";
+  fi
+else
+  echo "Please pass target dir name within ~/dump/out/ as second arg";
+  exit -1;
+fi
+
+tmp_dir_name="/home/ubuntu/dump/out/$2";
+tmp_file_name="$tmp_dir_name/simulate_$1.mos";
 
 cp "test.mos" $tmp_file_name;
 
@@ -25,12 +40,14 @@ echo "----------------------------------------";
 echo "----------------------------------------";
 date;
 
+# --indexReductionMethod=dummyDerivatives \
+cd $tmp_dir_name && \
 omc \
     -d=initialization,evaluateAllParameters \
-    --indexReductionMethod=dummyDerivatives \
     $tmp_file_name \
   | grep -v "Warning.*Connector.*\(KinematicPort\|WrenchPort\) is not balanced" \
-  > run.log;
+  > run.log && \
+cd -;
 
 echo "----------------------------------------";
 echo "----------------------------------------";
